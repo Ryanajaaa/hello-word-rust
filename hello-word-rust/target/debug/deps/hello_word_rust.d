2abdd93ad57f0dbd7ce1f1ffd69a24bafbcf39c8
RyanAndiyaS <161188252+Ryanajaaa@users.noreply.github.com>
2025-09-10 03:55:14 +0700

    Add files via upload

---
+++ hello-word-rust/target/debug/deps/hello_word_rust.d
@@ -0,0 +1,5 @@
+C:\Users\lenov\Desktop\Rust\hello-word-rust\target\debug\deps\hello_word_rust.d: src\main.rs
+
+C:\Users\lenov\Desktop\Rust\hello-word-rust\target\debug\deps\hello_word_rust.exe: src\main.rs
+
+src\main.rs:
---
+++ hello-word-rust/target/debug/hello-word-rust.d
@@ -0,0 +1,1 @@
+C:\Users\lenov\Desktop\Rust\hello-word-rust\target\debug\hello-word-rust.exe: C:\Users\lenov\Desktop\Rust\hello-word-rust\src\main.rs
